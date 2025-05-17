4dca1a690dd6d86e3fb83dd794fca265188945d2
finnsatoshi03 <finnsatoshi03@gmail.com>
2025-05-17 12:00:21 +0800

    feat: Enhance Forgot Password page with new layout and form component

--- src/features/auth/ForgotPasswordPage.tsx
+++ src/features/auth/ForgotPasswordPage.tsx
@@ -1,3 +1,22 @@
+import { Header } from "@/features/auth/components/header";
+import { Footer } from "@/features/auth/components/footer";
+import { ForgotPasswordForm } from "@/features/auth/components/forgot-password/forgot-password-form";
+
 export default function ForgotPasswordPage() {
-  return <div>ForgotPasswordPage</div>;
+  return (
+    <div className="flex h-full w-full flex-col justify-between">
+      <Header signIn />
+      <div className="mx-auto flex max-w-[450px] flex-col items-center justify-center gap-4 text-center">
+        <div className="space-y-1">
+          <h1 className="text-3xl font-bold">Forgot Your Password?</h1>
+          <p className="text-muted-foreground text-sm">
+            No worries — we’ll send you a secure link to reset your password.
+            Just enter your registered email address below.
+          </p>
+        </div>
+        <ForgotPasswordForm />
+      </div>
+      <Footer isForgotPassword />
+    </div>
+  );
 }
--- src/features/auth/components/footer.tsx
+++ src/features/auth/components/footer.tsx
@@ -1,8 +1,24 @@
 import { Link } from "react-router-dom";
+import { cn } from "@/lib/utils";
 
-export const Footer = () => {
+export const Footer = ({
+  isForgotPassword = false,
+}: {
+  isForgotPassword?: boolean;
+}) => {
   return (
-    <div className="flex w-full justify-end">
+    <div
+      className={cn(
+        "flex w-full justify-end",
+        isForgotPassword && "items-end justify-between",
+      )}
+    >
+      {isForgotPassword && (
+        <p className="text-muted-foreground max-w-[300px] text-xs">
+          Only registered company accounts can request a reset. For other
+          issues, please contact HR.
+        </p>
+      )}
       <div className="flex gap-6">
         <Link to="/privacy-policy">Privacy Policy</Link>
         <Link to="/support">Support</Link>

c7c7efe065b86eabe6b8b85b4d60d323e2f5542c
finnsatoshi03 <finnsatoshi03@gmail.com>
2025-05-17 12:54:34 +0800

    feat: Integrate React Query for data fetching and add authentication service for user management

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import AppRouter from "@/routes/AppRouter";
 import { ErrorBoundary } from "@/features/error";
 import { ThemeProvider } from "@/components/custom/theme-provider";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 0,
+    },
+  },
+});
+
 export default function App() {
   return (
-    <ThemeProvider>
-      <ErrorBoundary>
-        <BrowserRouter>
-          <AppRouter />
-        </BrowserRouter>
-      </ErrorBoundary>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <ThemeProvider>
+        <ErrorBoundary>
+          <BrowserRouter>
+            <AppRouter />
+          </BrowserRouter>
+        </ErrorBoundary>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
